22263f12f323898e0e54a553f3d69e9023ddb7d1
PREETHIMADDI <indumaddi9@gmail.com>
2023-10-22 20:20:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { WordCounter } from './components/wordcounter';
+function App() {
+  return (
+    <div className="App">
+    < WordCounter/>
+
+    </div>
+  );
+}
+
+export default App;
